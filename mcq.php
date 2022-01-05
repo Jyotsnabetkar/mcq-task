4716a7f09c89f8bc92c6a60d8d23688800661509
Jyotsnabetkar <40472438+Jyotsnabetkar@users.noreply.github.com>
2022-01-05 18:58:59 +0530

    Update mcq.php

--- mcq.php
+++ mcq.php
@@ -7,28 +7,33 @@ $json_data = file_get_contents("https://opentdb.com/api.php?amount=10");
  $response_data = json_decode($json_data);
  // All user data exists in 'data' object
  $user_data = $response_data->results;
+
 //print_r($user_data);
 //Traverse array and display user data
 foreach ($user_data as $mcq) {
-	echo "Question: ".$mcq->question;
+	echo "Question:".$mcq->question."<br/>";
 	echo "<br />";
 	
 		?>
 
 		<select>
+ <option value="<? echo $key; ?>">
+        	<?php 
+		$json_correct = $mcq->correct_answers;
+		$json_incorrect = $mcq->incorrect_answers;
 
-		<?  
-		$answer = $response_data->incorrect_answers;
-	
-		//print_r($answer);
-		foreach($answer as $key => $value){?>
+		print_r ($json_correct);
+		// decode json to array
+//$array[] = json_decode($json_correct, true);
+//$array[] = json_decode($json_incorrect, true);
 
-        <option value="<? echo $key; ?>">
-        	<? echo $value; ?>
+// encode array to json
+//$result = json_encode($array, JSON_PRETTY_PRINT);
+	//echo $result;		//var_dump($result);
+		?>
         		
         	</option>
-
-    <?  }   ?>
+		
 </select>
 	<?php } ?>
 
