6fe5a8e0ade2db25cfd7bdbb8f11591033810789
Jyotsnabetkar <40472438+Jyotsnabetkar@users.noreply.github.com>
2022-01-06 11:41:47 +0530

    Update mcq.php

--- mcq.php
+++ mcq.php
@@ -17,38 +17,22 @@ foreach ($user_data as $mcq) {
 		?>
 
 		<select>
- <option value="<? echo $key; ?>">
+			
+			<option>
         	<?php 
-
-		//echo $mcq['results']['category'];	
-				//$array[] = json_decode($response_data['correct_answer'], true);
-        		//$array[] = json_decode($response_data['incorrect_answers'], true);
-    //     		$data1 = json_encode(array['incorrect_answers'=>'5']); 
-				// $data2 = json_encode(array['correct_answer'=>'4']); 
-				// $results = json_encode(array_merge(json_decode($data1, true),json_decode($data2, true)));
-				// echo $results;
-        	foreach ($user_data as $array) {
-        				
-        		$result = json_encode($array, JSON_PRETTY_PRINT);
-				echo $result."<br/>".PHP_EOL;
-		}
-																			// foreach ($mcq['results'] as $correct_answer) {
-																			// 	echo $correct_answer['correct_answer']."<br>";
-																			// 	# code...
-																			// }
-		//print_r ($mcq);
-		// decode json to array
-// $array[] = json_decode($json_correct, true);
-// $array[] = json_decode($json_incorrect, true);
-
-// encode array to json
-//$result = json_encode($array, JSON_PRETTY_PRINT);
-	//echo $result;		
-		?>
-        		
-        	</option>
+        		$bucket = array();
+  
+        		$response_data = json_decode($json_data);
+				$user_data = $response_data->results;
+				$jsonobjnew = $mcq->correct_answer;
+				$jsonobj = $mcq->incorrect_answers;
+				array_push($jsonobj, $jsonobjnew);
+				print_r($jsonobj);
 		
-</select>
+        	?>
+        	</option>
+
+		</select>
 	<?php } ?>
 
 </html>
